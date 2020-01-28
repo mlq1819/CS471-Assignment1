edb63df72bc70924b38ef6a61c57992ac9dc07d6
Michael Quinn <mquinn5@binghamton.edu>
2020-01-28 14:41:49 -0500

    added powF

--- pow_full.c
+++ pow_full.c
@@ -9,6 +9,10 @@ int powI(int pow, int base) {
   return acc;
 } 
 
+int powF(int pow, int base, int current) {
+	return pow>1 ? powF(pow-1, current*base) : current;
+}
+
 int main(int argc, char **argv) {
   if (argc < 3) {
     printf("%s usage: [BASE] [POWER]\n", argv[0]);
@@ -16,7 +20,7 @@ int main(int argc, char **argv) {
   }
   int base = atoi(argv[1]);
   int pow = atoi(argv[2]);
-  int r = powI(pow,base);
+  int r = powF(pow,base,base);
   printf("%d\n", r);
   return 0;
 }
